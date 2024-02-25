b0aa4a70218df8b0ab0a327b7bb3ed5b0e1f4d35
Mubelotix <mubelotix@gmail.com>
2024-02-25 12:23:22 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,99 @@
+use proc_macro::{TokenStream, TokenTree};
+
+fn caseless_contains(text: &str, needle_lc: &str) -> bool {
+    if text.len() < needle_lc.len() {
+        return false;
+    }
+
+    let text = text.as_bytes();
+    let needle_lc = needle_lc.as_bytes();
+    'offset: for i in 0..text.len()-needle_lc.len() {
+        for j in 0..needle_lc.len() {
+            if text[i+j] != needle_lc[j] && (text[i+j] < 65 || text[i+j] > 90 || text[i+j] + 32 != needle_lc[j]) {
+                continue 'offset;
+            }
+        }
+        return true;
+    }
+    false
+}
+
+fn sql_to_code(sql: &str) -> String {
+    let mut code = String::new(); // TODO: Using a string is convenient, but it's not the fastest way to build a TokenStream
+    code.push('{');
+    code.push_str("let mut tx = pool.begin().await?;");
+
+    let mut vars = 0;
+    for query in sql.split(';').map(str::trim) {
+        match caseless_contains(query, "returning") {
+            true => {
+                let line = format!("let var{vars} = sqlx::query_as::<_, _>(\"{query};\").bind(ip).fetch_one(&mut *tx).await?;");
+                code.push_str(&line);
+                vars += 1;
+            },
+            false => {
+                let line = format!("sqlx::query(\"{query};\").bind(provider_id).execute(&mut *tx).await?;");
+                code.push_str(&line);
+            }
+        }
+    }
+    code.push_str("tx.commit().await?;" );
+    code.push_str(&format!("({})", (0..vars).map(|i| i.to_string()).collect::<Vec<_>>().join(", ")));
+    code.push('}');
+
+    code
+}
+
+//#[proc_macro_error]
+fn include_tx_inner(input: TokenStream) -> String {
+    let mut tokens = input.into_iter();
+    let lit = match tokens.next() {
+        Some(TokenTree::Literal(lit)) => lit,
+        Some(_) => panic!("The filename argument must be a string literal"),
+        None => panic!("Expected a filename argument")
+    };
+    if tokens.next().is_some() {
+        panic!("Expected only one argument")
+    }
+
+    let filename = lit.to_string();
+    if !filename.starts_with('"') || !filename.ends_with('"') {
+        panic!("The filename argument must be a string literal")
+    }
+    let filename = &filename[1..filename.len()-1];
+
+    let sql = std::fs::read_to_string(filename).unwrap_or_else(|_| {
+        panic!("Failed to read file {}", filename)
+    });
+
+    sql_to_code(&sql)
+}
+
+#[proc_macro]
+pub fn include_tx(input: TokenStream) -> TokenStream {
+    let code = include_tx_inner(input);
+    code.parse().expect("Internal error: bad code generated")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_caseless_contains() {
+        assert!(caseless_contains("Hello, world!", "hello"));
+        assert!(caseless_contains("Hello, world!", "world"));
+        assert!(caseless_contains("Hello, World!", "world"));
+        assert!(caseless_contains("Hello, WoRld!", "world"));
+
+        assert!(!caseless_contains("Hello, WoRld!", "foo"));
+        assert!(!caseless_contains("Hello, WoRld!", "WORLD"));
+    }
+
+    #[test]
+    fn test_codegen() {
+        let sql = "SELECT * FROM users; SELECT * FROM posts;";
+        let code = sql_to_code(sql);
+        println!("{}", code);
+    }
+}
